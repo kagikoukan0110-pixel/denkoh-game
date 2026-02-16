26693029b89c0fdf8754c92a87862c7d9aaa21f0
kagikoukan0110-pixel <kagikoukan0110@gmail.com>
2026-02-17 00:39:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ import { setupInteraction } from "./ui/interaction.js";
 import { Graph } from "./engine/graph.js";
 import { Scoring } from "./engine/scoring.js";
 
-alert("STEP4 OK");
-
 const graph = new Graph();
 const scoring = new Scoring(graph);
 
