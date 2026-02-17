6b0f04d5e4d3228422d05594f802ab213ee0d68a
kagikoukan0110-pixel <kagikoukan0110@gmail.com>
2026-02-17 12:20:42 +0900

    Update graph.js

--- engine/graph.js
+++ engine/graph.js
@@ -1,43 +1,5 @@
 export class Graph {
-
   constructor(){
-    this.s1State = 0;
-    this.s2State = 0;
-    this.breakerOn = false;
-
-    this.crimpDone = false;
-    this.crimpSize = null; // "small" | "medium" | "large"
-
-    this.wireCount = 2; // 今回の問題は2本接続
+    this.connections = [];
   }
-
-  toggleBreaker(){
-    this.breakerOn = !this.breakerOn;
-  }
-
-  toggleS1(){
-    this.s1State = this.s1State === 0 ? 1 : 0;
-  }
-
-  toggleS2(){
-    this.s2State = this.s2State === 0 ? 1 : 0;
-  }
-
-  setCrimp(size){
-    this.crimpDone = true;
-    this.crimpSize = size;
-  }
-
-  isLampOn(){
-
-    if(!this.breakerOn) return false;
-
-    const s1Output = this.s1State === 0 ? "T1" : "T2";
-    const s2Input  = this.s2State === 0 ? "T1" : "T2";
-
-    const circuitComplete = s1Output === s2Input;
-
-    return circuitComplete && this.crimpDone && this.crimpSize === "small";
-  }
-
 }
