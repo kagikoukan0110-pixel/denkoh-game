65675605f30dd4d5381da78cf3eb60ed89b5e653
kagikoukan0110-pixel <kagikoukan0110@gmail.com>
2026-02-17 09:24:06 +0900

    Update board.js

--- ui/board.js
+++ ui/board.js
@@ -3,8 +3,19 @@ export function initBoard(){
   const app = document.getElementById("app");
 
   app.innerHTML = `
-    <svg id="board" viewBox="0 0 800 500">
-      <circle id="lamp" cx="600" cy="200" r="20" fill="gray"/>
+    <svg viewBox="0 0 800 500">
+
+      <rect id="breaker" x="100" y="80" width="60" height="60" fill="#ccc"/>
+      <rect id="s1" x="250" y="150" width="60" height="60" fill="#ddd"/>
+      <rect id="s2" x="400" y="150" width="60" height="60" fill="#ddd"/>
+
+      <circle id="lamp" cx="650" cy="200" r="20" fill="gray"/>
+
     </svg>
+
+    <button id="checkBtn"
+      style="position:absolute; bottom:20px; left:20px;">
+      採点
+    </button>
   `;
 }
