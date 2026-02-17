3579e8aca5f2e9a52d738f3a3a5a8658e4118788
kagikoukan0110-pixel <kagikoukan0110@gmail.com>
2026-02-17 17:21:44 +0900

    Update circuitEngine.js

--- engine/circuitEngine.js
+++ engine/circuitEngine.js
@@ -1,29 +1,5 @@
-import { getThreeWayConnections } from "./switchLogic.js";
+import { buildGraph } from "./graph.js";
 
-export function buildCircuitGraph(devices, wires) {
-  const graph = {};
-
-  function add(a, b) {
-    if (!graph[a]) graph[a] = [];
-    graph[a].push(b);
-  }
-
-  // 外部配線
-  wires.forEach(w => {
-    add(w.from, w.to);
-    add(w.to, w.from);
-  });
-
-  // 三路内部導通
-  devices.forEach(d => {
-    if (d.type.startsWith("switch")) {
-      const connections = getThreeWayConnections(d);
-      connections.forEach(([a, b]) => {
-        add(a, b);
-        add(b, a);
-      });
-    }
-  });
-
-  return graph;
+export function buildCircuitGraph(devices){
+  return buildGraph(devices);
 }
