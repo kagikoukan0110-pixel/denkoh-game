92c5ff21d477119c2e44f28912845eace1321bc8
kagikoukan0110-pixel <kagikoukan0110@gmail.com>
2026-02-17 15:28:34 +0900

    Update switchLogic.js

--- engine/switchLogic.js
+++ engine/switchLogic.js
@@ -1,9 +1,24 @@
-export function getSwitchConnections(deviceEl) {
-  const mode = deviceEl.dataset.mode;
+// 三路スイッチ内部ロジック
 
-  if (mode === "left") {
-    return [["0", "1"]];
-  } else {
-    return [["0", "3"]];
+/**
+ * 三路スイッチの内部導通を返す
+ * @param {string} position  "left" or "right"
+ * @returns {Array} 接続されている端子ペア
+ */
+export function getThreeWayConnections(position) {
+  if (position === "left") {
+    return [
+      ["0", "1"],
+      ["1", "0"]
+    ];
   }
+
+  if (position === "right") {
+    return [
+      ["0", "3"],
+      ["3", "0"]
+    ];
+  }
+
+  return [];
 }
